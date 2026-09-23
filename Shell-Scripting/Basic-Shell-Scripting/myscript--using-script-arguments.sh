58e85ac879e1381cd0bf1382cb982fe7f9363ad7
Ajay Kumar Dwivedi <ajay.dwivedi2007@gmail.com>
2026-09-23 14:33:50 +0530

    updated shell script

--- Shell-Scripting/Basic-Shell-Scripting/myscript--using-script-arguments.sh
+++ Shell-Scripting/Basic-Shell-Scripting/myscript--using-script-arguments.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 # Purpose: Learning Shell Scripting
-#           Optional: Receive zero or more username from commandline. If no arguments provided, then ask user to provide one
+#          Optional: Receive zero or more username from commandline. If no arguments provided, then ask user to provide one
 # 2026-Sep-23 - Ajay Dwivedi - Initial Draft
+# ================================================
+# Help:
+#   man bash
+#   > Search for "CONDITIONAL EXPRESSIONS"
+# ================================================
+
 
 echo -n "The time is currently: "
 date
@@ -22,8 +28,13 @@ input_user=$*
 # check for zero length with test command
 if [ -z "$input_user" ]
 then
+  echo "command-line arguments not-detected."
+  echo
   echo -n "What user to query? "
   read input_user
+  echo
+else
+  echo "command-line arguments provided."
   echo
 fi
 
@@ -32,11 +43,17 @@ myuser=$(whoami)
 
 for user in $input_user;
 do
-  echo "********************** SESSION Output **********************"
+  echo "********************** SESSION Output for User [$user] **********************"
   echo "Looking for '$user' user last session as '$myuser'.."
   echo ''
 
-  last "$user" | grep "$user" | head -n 5
+  last_data=$(last "$user" | grep "$user" | head -n 5)
+  if [ -n "$last_data" ]
+  then
+    echo $last_data
+  else
+    echo "No last login data available for $user"
+  fi
 
   echo ''
   echo "Groups for '$user' user.."
@@ -44,3 +61,21 @@ do
   echo
 done
 
+echo
+echo -n "Enter list of files to check: "
+read input_files
+echo
+
+for file in $input_files;
+do
+  echo "****** Working on file [$file] ********"
+  if [ -e $file ] && [ -r $file ]; then
+    #echo "File $file exists and is readable"
+    ls -l $file
+  elif [ -e $file ]; then
+    echo "File $file is not readable"
+  else
+    echo "File $file does not exist."
+  fi
+done
+echo
